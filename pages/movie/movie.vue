4c0b31030d41fd4eaf35853db0002a56f27870a9
luoyunlong <=>
2023-03-13 13:22:24 +0800

    添加了个div

--- pages/movie/movie.vue
+++ pages/movie/movie.vue
@@ -1,25 +1,5 @@
 <template>
-	<view class="wrap">
-		<u-row gutter="16">
-			<u-col span="3">
-				<view class="demo-layout bg-purple"></view>
-			</u-col>
-			<u-col span="4">
-				<view class="demo-layout bg-purple-light"></view>
-			</u-col>
-			<u-col span="5">
-				<view class="demo-layout bg-purple-dark"></view>
-			</u-col>
-		</u-row>
-		<u-row gutter="16" justify="space-between">
-			<u-col span="3">
-				<view class="demo-layout bg-purple"></view>
-			</u-col>
-			<u-col span="9">
-				<view class="demo-layout bg-purple-light"></view>
-			</u-col>
-		</u-row>
-	</view>
+	<view class="box">123</view>
 </template>
 
 <script>
@@ -39,30 +19,9 @@
 </script>
 
 <style scoped lang="scss">
-	// @import "uview-ui/index.scss";
-
-	.wrap {
-		padding: 24rpx;
-	}
-
-	.u-row {
-		margin: 40rpx 0;
-	}
-
-	.demo-layout {
-		height: 80rpx;
-		border-radius: 8rpx;
-	}
-
-	.bg-purple {
-		background: #d3dce6;
-	}
-
-	.bg-purple-light {
-		background: #e5e9f2;
-	}
-
-	.bg-purple-dark {
-		background: #99a9bf;
+	.box{
+		width: 100%;
+		height: 300px;
+		background: blue;
 	}
 </style>
